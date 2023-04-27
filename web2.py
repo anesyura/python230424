28fb87f2e347b2317f9ca14def8889461d61d3ed
110anes <110anes@naver.com>
2023-04-27 15:54:43 +0900

    당근마켓에 UI까지 처리

--- web2.py
+++ web2.py
@@ -3,10 +3,10 @@
 import requests
 #크롤링
 from bs4 import BeautifulSoup
+
 url = "https://www.daangn.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-
 posts = soup.find_all("div", attrs={"class":"card-desc"})
 # 파일에 저장
 f = open("c:\\work\\daangn.txt","wt",encoding="utf-8")
